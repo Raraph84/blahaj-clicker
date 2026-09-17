c66f53b3fa9dc904ed7d6e502c357adeecfdd57a
Raraph84 <contact@raraph.fr>
2026-09-17 18:37:47 +0200

    Add rate limit

--- src/websocket.ts
+++ src/websocket.ts
@@ -2,14 +2,15 @@ import Elysia from "elysia";
 import { ElysiaWS } from "elysia/dist/ws";
 import { getCount, updateCount } from "./utils/database";
 
-const clients: { [key: string]: { ws: ElysiaWS, ip: string } } = {};
+const clients: { [key: string]: { ws: ElysiaWS, ip: string, clickTimestamps: number[] } } = {};
 
 export default new Elysia({ prefix: "/websocket", websocket: { idleTimeout: 20 * 60 } })
     .ws("/", {
         open(ws) {
             const client = clients[ws.id] = {
                 ws,
-                ip: ws.data.headers["x-forwarded-for"] ?? ws.remoteAddress
+                ip: ws.data.headers["x-forwarded-for"] ?? ws.remoteAddress,
+                clickTimestamps: []
             };
 
             console.log(`${client.ip} connected to the websocket. (Current connections: ${Object.keys(clients).length})`);
@@ -21,6 +22,13 @@ export default new Elysia({ prefix: "/websocket", websocket: { idleTimeout: 20 *
         message(ws, message) {
             if (message === "ping") return ws.send("pong");
             if (message !== "click") return;
+
+            const client = clients[ws.id];
+
+            const now = Date.now();
+            client.clickTimestamps = client.clickTimestamps.filter((t) => now - t < 1000);
+            if (client.clickTimestamps.length >= 10) return;
+            client.clickTimestamps.push(now);
 
             const count = getCount() + 1;
             updateCount(count);
